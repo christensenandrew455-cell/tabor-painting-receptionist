e23b8148862c9511b38d2c66ecac3807495f5a66
christensenandrew455-cell <christensenandrew455@gmail.com>
2026-08-04 12:48:40 -0400

    Remove unused turn settings

--- modular-models.js
+++ modular-models.js
@@ -10,8 +10,6 @@ export const MODELS = Object.freeze({
 export const TURN = Object.freeze({
   silenceMs: Number(process.env.AI_SILENCE_MS || 1500),
   prefixPaddingMs: Number(process.env.AI_PREFIX_PADDING_MS || 300),
-  bargeInMs: Number(process.env.AI_BARGE_IN_MS || 120),
-  maxReplyCharacters: Number(process.env.AI_MAX_REPLY_CHARACTERS || 280),
 });
 
 export const LEAD_FIELDS = Object.freeze([
